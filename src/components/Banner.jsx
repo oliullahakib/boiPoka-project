ddb3b68fe21b17e32661eaf1d424dc4daa0d181a
oliullahakib <oliullahakib@gmail.com>
2025-10-05 11:53:33 +0600

    make banner a new component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import heroImg from "../assets/books.jpg"
+const Banner = () => {
+    return (
+        <div className="hero bg-gray-200  w-11/12 mx-auto rounded-2xl py-8 md:py-20 mt-12">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img
+                    src={heroImg}
+                    className=" md:max-w-lg rounded-lg shadow-2xl"
+                />
+                <div>
+                    <h1 className="text-3xl md:text-5xl font-bold mb-12"> Books to freshen up your bookshelf</h1>
+                    <button className="btn btn-success text-white font-bold">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
