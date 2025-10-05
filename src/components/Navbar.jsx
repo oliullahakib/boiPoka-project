2a8fb3758204fc6143a12a79d85d566864ce4b73
oliullahakib <oliullahakib@gmail.com>
2025-10-05 11:49:50 +0600

    all the initial file added and banner section added to home page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+
+const Navbar = () => {
+    const links = [
+        <NavLink to={'/'}>Home</NavLink>,
+        <NavLink to={'/listedBooks'}>Listed Books</NavLink>,
+        <NavLink to={'/pages'}>Pages to Read</NavLink>
+    ]
+    return (
+        <div className="navbar bg-base-100  w-11/12 mx-auto">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        {links}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">Book Vibe</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal space-x-5 px-1">
+                    {
+                     links
+                    }
+                </ul>
+            </div>
+            <div className="navbar-end flex-col md:flex-row gap-2 md:gap-5">
+                <a className="btn btn-success text-white">Sing In</a>
+                <a className="btn btn-info text-white">Sing Up</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
